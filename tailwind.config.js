3d0072176cf29b166a5d9031b4f23d9aa46b3a2e
Rcnald <ronaldomjunior05@gmail.com>
2023-03-02 22:54:38 -0300

    process

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,36 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        'primary':['Nunito, sans-serif'],
+      },
+      colors:{
+        'gray':{
+          'bg':'#e5e5e5',
+          'text':'#363636',
+        },
+        'blue':{
+          'btn-primary':'#2d849f',
+          'btn-primary-hover':'#3da6c8',
+        },
+        'instagram':{
+          'bg':'#e4405f',
+          'btn':'#4f5bd5',
+          'btn-hover':'#646eda',
+        },
+        'linkedin':{
+          'bg':'#0077b5',
+          'btn':'#FFF',
+          'btn-hover':'#d9d9d9',
+        },
+        'twitter':{
+          'bg':'#00aced',
+          'btn':'#FFF',
+          'btn-hover':'#d9d9d9',
+        },
+      },
+    },
   },
   plugins: [],
 }
